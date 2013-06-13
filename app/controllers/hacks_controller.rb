c629dbdb0d059441deece049bb58818d83425a60
Alex Annese <alex@yammer-inc.com>
2013-06-13 14:51:41 -0700

    added contributors

--- app/controllers/hacks_controller.rb
+++ app/controllers/hacks_controller.rb
@@ -1,6 +1,6 @@
 class HacksController < ApplicationController
 
-  before_filter :get_hack, :only => [:upvote, :downvote, :show]
+  before_filter :get_hack, :only => [:upvote, :downvote, :show, :add_contributor]
 
   def index
     @hacks = Hack.order("votes DESC")
@@ -13,8 +13,8 @@ class HacksController < ApplicationController
     if current_user.bankroll <= 0
       flash[:error] = "no more votes left to give!"
     else
-      flash[:message] = "you have cast your vote!"
       @hack.upvote(current_user)
+      flash[:message] = "you have cast your vote!"
     end
     redirect_to :back
   end
@@ -23,8 +23,28 @@ class HacksController < ApplicationController
     if current_user.bankroll <= 0
       flash[:error]   = "no more votes left to give!"
     else
-      flash[:message] = "you are a terrible person"
       @hack.downvote(current_user)
+      flash[:message] = "you are a terrible person"
+    end
+    redirect_to :back
+  end
+
+  def add_contributor
+    if @hack.has_contributor?(current_user)
+      flash[:error] = "you're already a contributor!"
+    else
+      @hack.add_contributor(current_user)
+      flash[:message] = "we recognize you for your efforts, thank you"
+    end
+    redirect_to :back
+  end
+
+  def remove_contributor
+    if !@hack.has_contributor?(current_user)
+      flash[:error] = "what are you doing? you aren't a contributor!"
+    else
+      @hack.remove_contributor(current_user)
+      flash[:message] = "you are no longer a contributor :("
     end
     redirect_to :back
   end

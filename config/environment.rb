169b1e6e93d6aa69f8ca51371ca6a88d30ca996a
Kane Ren <kren@yammer-inc.com>
2013-06-14 11:01:19 -0700

    fix shit

--- config/environment.rb
+++ config/environment.rb
@@ -9,13 +9,13 @@ require 'pp'
 require 'yammer'
 require 'yaml'
 
-APP_CONFIG = YAML.load_file("#{Rails.root}/config/app_config.yml")
+#APP_CONFIG = YAML.load_file("#{Rails.root}/config/app_config.yml")
 
-YAMMER = Yammer::Client.new(
-  :client_id     => APP_CONFIG['YMR_CLIENT_ID'],
-  :client_secret => APP_CONFIG['YMR_CLIENT_SECRET'],
-  :access_token  => APP_CONFIG['ACCESS_TOKEN']
-)
+#YAMMER = Yammer::Client.new(
+#  :client_id     => APP_CONFIG['YMR_CLIENT_ID'],
+#  :client_secret => APP_CONFIG['YMR_CLIENT_SECRET'],
+#  :access_token  => APP_CONFIG['ACCESS_TOKEN']
+#)
 
 CONNECTION             = Net::HTTP.new('yammer.com', 443)
 CONNECTION.use_ssl     = true

6dc08f379557ccf56d742400b9ef8e1bb4b283d9
Kevin Davis <kgdavis@gmail.com>
2013-06-13 20:09:49 -0700

    layout updates and local dev logged in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,10 @@ class ApplicationController < ActionController::Base
 
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
+
+    if Rails.env.development?
+    	@current_user = User.first
+    end 
   end
 
   helper_method :current_user

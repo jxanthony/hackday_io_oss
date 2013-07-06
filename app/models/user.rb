d890b63740e311e53a83763917dd99053e554362
Kane Ren <kren@yammer-inc.com>
2013-07-05 22:56:13 -0700

    more oauth

--- app/models/user.rb
+++ app/models/user.rb
@@ -21,14 +21,18 @@ class User < ActiveRecord::Base
   has_many :activities
 
   def self.from_omniauth(auth)
-    where(auth.slice(:provider, :uid)).first_or_initialize.tap do |user|
+    user = self.where(:provider => auth.provider, :uid => auth.uid)
+    if user.empty?
+      user = User.new
       user.provider = auth.provider
       user.uid = auth.uid
       user.name = auth.info.name
       user.oauth_token = auth.credentials.token
-      user.oauth_expires_at = Time.at(auth.credentials.expires_at)
       user.save!
+    else
+      user = user.first
     end
+    user
   end
 
 end

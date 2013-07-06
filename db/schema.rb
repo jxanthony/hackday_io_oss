711d74342b26c0cc64bd3c79428d71c307ef5994
Kane Ren <kren@yammer-inc.com>
2013-07-05 22:37:24 -0700

    more oauth

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130702003409) do
+ActiveRecord::Schema.define(:version => 20130706053636) do
 
   create_table "activities", :force => true do |t|
     t.integer  "user_id"
@@ -62,8 +62,8 @@ ActiveRecord::Schema.define(:version => 20130702003409) do
     t.integer  "bankroll",         :default => 10
     t.datetime "created_at",                       :null => false
     t.datetime "updated_at",                       :null => false
-    t.string   "uid"
     t.boolean  "admin"
+    t.integer  "uid"
   end
 
 end

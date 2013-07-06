cf8d93e1bbf74f4fa4db9e5ffe4a3f685c8f758b
Kane Ren <kren@yammer-inc.com>
2013-07-05 22:44:52 -0700

    oauth debug

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,6 @@
 class SessionsController < ApplicationController
   def create
-    user = User.from_omniauth(env["omniauth.auth"])
+    #user = User.from_omniauth(env["omniauth.auth"])
     #session[:user_id] = user.id
     auth = request.env['omniauth.auth']
     render :json => auth.to_json

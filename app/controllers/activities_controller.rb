c71d06be1d4d8716427e90cb973718e040f02ce9
Kane Ren <kren@yammer-inc.com>
2013-07-08 21:50:12 -0700

    limit recent activity to 10 items instead of 20

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,6 +1,6 @@
 class ActivitiesController < ApplicationController
   def index
-    results = Activity.order("created_at DESC").limit(20).reverse!
+    results = Activity.order("created_at DESC").limit(10).reverse!
 
     render :json => results.to_json(:include => [:user, :hack]), :status => :ok
   end

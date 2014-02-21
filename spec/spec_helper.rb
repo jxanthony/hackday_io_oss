1cebf18354fac519bbe010e267a869e9d03f96db
Kevin Davis <kgdavis@gmail.com>
2014-02-21 00:28:06 -0800

    Fix code coverage

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,9 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV["RAILS_ENV"] ||= 'test'
+
+require 'simplecov'
+SimpleCov.start 'rails'
+
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 require 'rspec/autorun'
@@ -11,10 +15,6 @@ Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
 require 'capybara/rspec'
 require 'capybara/rails'
 Capybara.javascript_driver = :webkit
-
-require 'database_cleaner'
-require 'simplecov'
-SimpleCov.start 'rails'
 
 OmniAuth.config.test_mode = true
 OmniAuth.config.mock_auth[:yammer] = {
@@ -59,16 +59,14 @@ RSpec.configure do |config|
   config.order = "random"
 
   # == Clean up after integration tests
+  require 'database_cleaner'
+
   config.before(:suite) do
     DatabaseCleaner.strategy = :truncation
     DatabaseCleaner.clean_with(:truncation)
   end
 
   config.before(:each) do
-    DatabaseCleaner.start
-  end
-
-  config.after(:each) do
     DatabaseCleaner.clean
   end
 

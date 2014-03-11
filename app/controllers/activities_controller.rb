d0baa19421fc66ebee38adc56f70ffad59291a80
Kane Ren <kren@yammer-inc.com>
2014-03-10 17:38:59 -0700

    activities fix

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -6,6 +6,6 @@ class ActivitiesController < ApplicationController
       scope = Hackday.find(params[:hackday_id])
     end
 
-    render scope.activities.where("activities.id > #{params[:cursor] || 0}").order("created_at DESC").limit(12).reverse!
+    render scope.activities.where("activities.id > #{params[:cursor] || 0}").order("created_at DESC").limit(20)
   end
 end

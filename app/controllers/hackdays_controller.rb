231a1a2c2a67aa19a5ce4d4e1f665a2ee41adc81
Kevin Davis <kgdavis@gmail.com>
2014-03-26 02:26:17 -0700

    Edit / Destroy Hack Days

--- app/controllers/hackdays_controller.rb
+++ app/controllers/hackdays_controller.rb
@@ -11,6 +11,28 @@ class HackdaysController < ApplicationController
     end
   end
 
+  def update
+    hackday = Hackday.find(params[:id])
+    hackday.update_attributes(params[:hackday])
+    if hackday.errors.any?
+      flash[:error] = hackday.errors.full_messages.join(", ")
+      redirect_to hackday
+    else
+      flash[:message] = "Updated your Hack Day"
+      redirect_to hackday
+    end
+  end
+
+  def destroy
+    hackday = Hackday.find(params[:id])
+    if hackday.destroy
+      flash[:message] = "Your Hack Day has been destroyed. This is a sad day."
+      redirect_to root_url
+    else
+      flash[:error] = "There was some kind of problem destroying your Hack Day - perhaps the server couldn't bear the thought of destroying a Hack Day"
+    end
+  end
+
   def show
     @hackday = Hackday.find(params[:id])
     @hacks = @hackday.hacks.order("votes DESC")

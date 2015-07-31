bff724b543c43540491488c0970d3f1fd78c81ae
Grace Chang <gchang@yammer-inc.com>
2015-07-30 22:41:29 -0700

    Fix bug with breaks

--- app/models/hackday.rb
+++ app/models/hackday.rb
@@ -28,15 +28,15 @@ class Hackday < ActiveRecord::Base
   end
 
   def queue_for_display
-    queue.first(4)
+    queue(false).first(4)
   end
 
   def hack_at_index(num)
-    queue.detect{ |hack| hack.presentation_index == num}
+    queue(false).detect{ |hack| hack.presentation_index == num}
   end
 
   def join_queue(hack)
-    hack.update_attribute(:presentation_index, queue.size + 1)
+    hack.update_attribute(:presentation_index, queue(false).size + 1)
   end
 
   def leave_queue(hack)
@@ -61,7 +61,7 @@ class Hackday < ActiveRecord::Base
   end
 
   def move_down_in_queue(hack)
-    return false if hack.presentation_index.nil? or hack.presentation_index + 1 > queue.size
+    return false if hack.presentation_index.nil? or hack.presentation_index + 1 > queue(false).size
 
     swap_positions(hack.presentation_index + 1, hack.presentation_index)
     create_activity(hack, 'move_down_in_queue')
@@ -70,7 +70,7 @@ class Hackday < ActiveRecord::Base
   def add_break(position)
     breaktime = Breaktime.new
     breaktime.hackday = self
-    breaktime.presentation_index = queue.size + 1
+    breaktime.presentation_index = queue(false).size + 1
     breaktime.save!
 
     swap_positions(breaktime.presentation_index, position) if position
@@ -107,7 +107,7 @@ class Hackday < ActiveRecord::Base
 
   def bump_queue(index)
     # FIXME: this is absolutely disgusting
-    queue.where("presentation_index > #{index}").each do |hack|
+    queue(false).where("presentation_index > #{index}").each do |hack|
       former_index = hack.presentation_index
       hack.update_attribute(:presentation_index, former_index - 1)
     end
